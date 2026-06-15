0c7ccd0669545fb5ac3a30b78a9be11d397475d3
Maedeh <maedeh.soleymani.dev@gmail.com>
2026-06-15 16:38:11 +0330

    ready for deploy2

--- pasnevesht/settings.py
+++ pasnevesht/settings.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
-from gitignore.email_config import email_host_pass, email_host_user, default_from_email
-from gitignore.secret_key import secret_key
+# from gitignore.email_config import email_host_pass, email_host_user, default_from_email
+# from gitignore.secret_key import secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret_key
+# SECRET_KEY = secret_key
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -126,8 +127,6 @@ AUTH_USER_MODEL = 'accounts.User'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [BASE_DIR / 'static',]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
@@ -140,6 +139,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT= 587
 EMAIL_USE_TLS= True
-EMAIL_HOST_USER= email_host_user
-EMAIL_HOST_PASSWORD= email_host_pass
-DEFAULT_FROM_EMAIL= default_from_email
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
+
+# EMAIL_HOST_USER= email_host_user
+# EMAIL_HOST_PASSWORD= email_host_pass
+# DEFAULT_FROM_EMAIL= default_from_email
