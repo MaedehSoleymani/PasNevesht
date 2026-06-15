9307b1d449d7b2794c61228bf966155cb704749e
Maedeh <maedeh.soleymani.dev@gmail.com>
2026-06-15 16:49:24 +0330

    fixing static root

--- pasnevesht/settings.py
+++ pasnevesht/settings.py
@@ -126,7 +126,7 @@ AUTH_USER_MODEL = 'accounts.User'
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
